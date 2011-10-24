ab28efcfc8c3e3759cbec52b8cb588f4d8a12d96
Javier Jardón <jjardon@gnome.org>
2011-10-24 03:06:04 +0100

    autogen.sh: Improve script
    Also remove INSTALL file as Its autogenerated

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,27 @@
 #! /bin/sh
-gtkdocize --flavour no-tmpl || exit 1
-ACLOCAL="${ACLOCAL-aclocal} $ACLOCAL_FLAGS" autoreconf -v --install || exit 1
-glib-gettextize --force --copy || exit 1
-./configure --enable-debug "$@"
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+        echo "*** No GTK-Doc found, please install it ***"
+        exit 1
+fi
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install it ***"
+        exit 1
+fi
+
+mkdir -p m4
+
+gtkdocize || exit $?
+autoreconf --force --install --verbose || exit $?
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
