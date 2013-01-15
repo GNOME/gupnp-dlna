7b8fade6ecd5921facf031ebd1b37683646efbb6
Jens Georg <jensg@openismus.com>
2013-01-15 16:53:05 +0100

    Add single include header file

---
+++ libgupnp-dlna/gupnp-dlna.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Intel Corporation.
+ *
+ * Authors: Jens Georg <jensg@openismus.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "gupnp-dlna-profile-guesser.h"
+#include "gupnp-dlna-profile.h"
+#include "gupnp-dlna-restriction.h"
+#include "gupnp-dlna-value-list.h"
+#include "gupnp-dlna-g-values.h"
+#include "gupnp-dlna-audio-information.h"
+#include "gupnp-dlna-container-information.h"
+#include "gupnp-dlna-image-information.h"
+#include "gupnp-dlna-video-information.h"
+#include "gupnp-dlna-information.h"
+#include "gupnp-dlna-values.h"
